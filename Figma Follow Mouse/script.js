5e6930cfb02804ca27e98fa85625325a619e1b7b
Ashish Kumar <ashish902760@gmail.com>
2023-09-25 18:36:22 +0530

    Add Figma Follow Mouse

---
+++ Figma Follow Mouse/script.js
@@ -0,0 +1,12 @@
+
+
+let figmaIcon = document.getElementById("figma-icon");
+
+function move(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    figmaIcon.style.left = x - 50 + "px"; // Adjust the position to center the image on the cursor
+    figmaIcon.style.top = y - 50 + "px"; // Adjust the position to center the image on the cursor
+}
+
+document.addEventListener("mousemove", move);
